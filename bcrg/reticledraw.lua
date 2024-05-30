e834bd7c3b0e275bb12f83f2f5648c0396d6b8b3
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-05-30 15:41:55 +0300

    first render

--- bcrg/reticledraw.lua
+++ bcrg/reticledraw.lua
@@ -28,12 +28,12 @@ function ReticleDraw:c_fill_rect(x, y, width, height, color)
 end
 
 function ReticleDraw:c_pixel(x, y, color)
-    self:pixel(self.cx + x, self.cy + y, color)
+    self:pixel(math.ceil(self.cx + x), math.ceil(self.cy + y), color)
 end
 
 function ReticleDraw:c_hline(x, y, width, color)
     self:hline(
-            (self.cx + x) - width,
+            (self.cx + x),
             (self.cy + y),
             width,
             color
@@ -43,7 +43,7 @@ end
 function ReticleDraw:c_vline(x, y, height, color)
     self:vline(
             (self.cx + x),
-            (self.cy + y) - height,
+            (self.cy + y),
             width,
             color
     )
@@ -67,10 +67,41 @@ end
 
 function ReticleDraw:c_line(x0, y0, x1, y1, color)
     self:line(
-            (self.cx + x0),
-            (self.cy + y0),
-            (self.cx + x1),
-            (self.cy + y1),
+            math.ceil(self.cx + x0),
+            math.ceil(self.cy + y0),
+            math.ceil(self.cx + x1),
+            math.ceil(self.cy + y1),
             color
     )
+end
+
+function ReticleDraw:c_ellipse(x, y, rx, ry, color)
+    self:ellipse_by_center(self.cx + x, self.cy + y, rx, ry, color)
+end
+
+function ReticleDraw:c_fill_ellipse(x, y, rx, ry, color)
+    self:c_ellipse(self.cx + x, self.cy + y, rx, ry, color)
+end
+
+function ReticleDraw:c_circle(x, y, r, color)
+    self:ellipse_by_center(self.cx + x, self.cy + y, r, r, color)
+end
+
+function ReticleDraw:c_fill_circle(x, y, r, color)
+    self:fill_ellipse_by_center(self.cx + x, self.cy + y, r, r, color)
+end
+
+
+function make_canvas(width, height, bit_depth)
+    local buffer_size = (width * height * bit_depth) / 8
+
+    -- Create a buffer for the display
+    local buf = {}
+    for i = 1, buffer_size do
+        buf[i] = 0
+    end
+
+    -- Initialize the frame buffer
+    local fb = ReticleDraw:new(buf, width, height, bit_depth == 1 and MVLSB or NMLSB)
+    return fb
 end
